c78a2bbecb83e1da302b01a64224031fc9b5568f
ayushbisht <ayushking6395@gmail.com>
2025-09-19 13:18:39 +0530

    front-end

---
+++ front-end/src/page/stats.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Stats = () => {
+  return (
+    <div className="stats-container">
+      <h1>Statistics Page</h1>
+      <p>This is the statistics page of your application.</p>
+    </div>
+  )
+}
+
+export default Stats
